d66cfec764960f90bdae11b007f368418ffe4cdc
Arthur Maurer <30410972+maurerarthur@users.noreply.github.com>
2020-07-28 00:58:03 -0300

    Connection factory created

--- src/routes/Create.js
+++ src/routes/Create.js
@@ -1,7 +1,15 @@
 const Router = require("express").Router();
+const database = require("../../ConnectionFactory.js");
 
-Router.get("/", (req, res) => {
+let Connection = new database();
+
+Router.get("/", async (req, res) => {
+
+    const conn = await Connection.Mongo();
+    console.log(conn.db("crud").collection("things"));
+
     res.send("Create");
+
 });
 
 module.exports = Router;
