f6e521905848df6d3ec8bf258f8d5fa4b4c3070d
Arthur Maurer <30410972+maurerarthur@users.noreply.github.com>
2020-07-31 01:42:36 -0300

    Delete finished

--- src/routes/Delete.js
+++ src/routes/Delete.js
@@ -1,7 +1,17 @@
 const Router = require("express").Router();
+const Mongo = require("mongodb");
+const database = require("../../ConnectionFactory.js");
 
-Router.get("/", (req, res) => {
-    res.send("Delete");
+const Connection = new database();
+
+Router.post("/", async (req, res) => {
+
+    const conn = await Connection.Mongo();
+
+    conn.db("crud").collection("products").deleteOne({ _id: new Mongo.ObjectID(req.body.deletedProduct) }).then(() => {
+        res.redirect("/");
+    });
+
 });
 
 module.exports = Router;
