43dd129725cda3ad8fb2d910e357dd068db14183
Arthur Maurer <30410972+maurerarthur@users.noreply.github.com>
2020-08-04 00:48:09 -0300

    Update finished

--- src/routes/Update.js
+++ src/routes/Update.js
@@ -1,7 +1,40 @@
 const Router = require("express").Router();
+const path = require("path");
+const Mongo = require("mongodb");
+const database = require("../../ConnectionFactory.js");
 
-Router.get("/", (req, res) => {
-    res.send("Update");
+const Connection = new database();
+
+Router.get("/", async (req, res) => {
+
+    const conn = await Connection.Mongo();
+
+    conn.db("crud").collection("products").findOne(new Mongo.ObjectID(req.query.id), (err, result) => {
+
+        if(err) {
+            throw err;
+        }
+
+        res.render(path.join(__dirname, "../views", "Update.ejs"), { product: result });
+
+    });
+
+});
+
+Router.post("/", async (req, res) => {
+
+    const conn = await Connection.Mongo();
+
+    conn.db("crud").collection("products").updateOne({ _id: new Mongo.ObjectID(req.body.id) }, {
+        $set: {
+            product: req.body.product,
+            description: req.body.description,
+            quantity: req.body.quantity
+        }
+    }).then(() => {
+        res.redirect("/");
+    });
+
 });
 
 module.exports = Router;
