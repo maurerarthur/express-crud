7e81ef0fc1d0789ce01b9ec8abbd4b312f9ee27d
Arthur Maurer <30410972+maurerarthur@users.noreply.github.com>
2020-08-04 00:07:25 -0300

    The system now shows a friendly message if there are no products registered

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 
-//Routes
-
 const index = require("./src/routes/Index");
 const create = require("./src/routes/Create");
 const update = require("./src/routes/Update");
 const del = require("./src/routes/Delete");
 
-//-----
-
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
+//Routes
 
 app.use("/", index);
 app.use("/create", create);
 app.use("/update", update);
 app.use("/delete", del);
+
+//-----
 
 app.listen(3000);
